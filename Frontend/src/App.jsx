bb815b0f7d7ca62bf5bf69426285fb8160424435
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-05-16 17:22:48 -0300

    login v1.0

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import LandingPage from "./pages/LandingPage/LandingPage";
+// import LandingPage from "./pages/LandingPage/LandingPage";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <Login/>
     </>
   );
 }
