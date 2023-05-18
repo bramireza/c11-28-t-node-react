e226fa0facf3a6658332481eba8a7845cbb6ab4c
pablo-zalazar <pablo_zalazar@outlook.com>
2023-05-18 16:44:56 -0300

    normalize css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-
 import NavBar from "./components/Navbar/Navbar";
-import LandingPage from "./pages/LandingPage/LandingPage";
 import PruebaRutas from "./components/PruebaRutas/PruebaRutas";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import Registro from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="registro" element={<Registro />} />
           <Route path="/resetPassword" element={<ResetPassword />} />
           {/* aca se va a poner la ruta al login y el componente de login, el componente pruba es solo para probar */}
           <Route path="/prueba" element={<PruebaRutas />} />
