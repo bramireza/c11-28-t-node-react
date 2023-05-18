1fab5ae0e69b4b417fda645cac26f9274f6c9361
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-05-18 15:12:53 -0500

    Se hizo el login.controller

---
+++ Backend/src/controllers/login.controllers.js
@@ -0,0 +1,24 @@
+const Patient = require('../models/Patient')
+const bcrypt = require('bcrypt')
+
+const loginUser = async (req,res) => {
+    const {email, password} = req.body
+
+    try {
+        const patient = Patient.findOne({email})
+        if (!patient) {
+            return res.status(400).json({message:'Credenciales Invalidas'})
+        }
+
+        const isMatch = await bcrypt.compare(password, patient.password)
+        if (!isMatch) {
+            return res.status(400).json({message:'Credenciales Invalidas'})
+        }
+    } catch (error) {
+        res.status(500).json({message:'Error del servidor'})
+    }
+}
+
+module.exports = {
+    loginUser
+}
