9e044480bb0b50a541064e382dcef96f7570965d
SenDan07 <dany.alekrmz@gmail.com>
2023-05-19 04:32:07 -0700

    Medic model renamed to doctor, export naming updated

--- Backend/src/models/Doctor.js
+++ Backend/src/models/Doctor.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const medicSchema = new Schema({
+const doctorSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -30,19 +30,17 @@ const medicSchema = new Schema({
           type: Schema.Types.ObjectId,
           ref: 'Specialty',
           required: true,
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   schedule: {
-    type: String,
-    required: true,
+    
   },
   active: {
     type: Boolean,
     default: true,
   },
-
 });
 
-module.exports = mongoose.model('Medic', medicSchema);
+module.exports = mongoose.model('Doctor', doctorSchema);
