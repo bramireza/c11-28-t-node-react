c2f18e0ca4b09dbb2eb78ef41edf317acaee8213
Leandro Calderón <mellyleo17@gmail.com>
2023-05-19 00:58:16 -0300

    componente simplificado, falta estetica

--- Frontend/src/components/PopUpConfirm.jsx
+++ Frontend/src/components/PopUpConfirm.jsx
@@ -1,5 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+// import Styles from "./PopUpConfirm.module.css;"
 
 function PopUpConfirm(props) {
   return (
