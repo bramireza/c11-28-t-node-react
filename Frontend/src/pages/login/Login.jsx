eb69eb7b81eddfaf9d4dc913f98a0fc46d911ad8
pablo-zalazar <pablo_zalazar@outlook.com>
2023-05-20 11:23:25 -0300

    login and register

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -1,29 +1,101 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useServices from "../../services/useServices";
+import validatePassword from "../../utilities/validatePassword";
+import style from "./login.module.css";
+
+const initialValues = {
+  personalId: "",
+  password: "",
+};
 
 const Login = () => {
+  const [state, setState] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+
+  const { auth } = useServices();
+
+  const validate = () => {
+    let errorsList = {};
+
+    if (!state.personalId) errorsList = { ...errorsList, personalId: "Campo obligatorio" };
+    else if (state.personalId.length < 8) errorsList = { ...errorsList, personalId: "Debe tener al menos 8 caracteres" };
+
+    const errorPass = validatePassword(state.password);
+    if (errorPass) errorsList = { ...errorsList, password: errorPass };
+
+    return errorsList;
+  };
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+    const response = validate();
+    console.log(response);
+    if (Object.keys(response).length < 1) {
+      const body = {
+        personalId: state.personalId,
+        password: state.password,
+      };
+      console.log(body);
+      try {
+        const { data } = await auth.login(body);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setErrors(response);
+  };
+
   return (
     <main className="mx-auto text-center w-75">
       <h1 className="pt-4">Inicio de Sesion</h1>
       <h4 className="mb-5">Ingresar a tu cuenta</h4>
-      <form action="" className="d-flex flex-column gap-2">
-        <label htmlFor="" className="fst-italic align-self-start">
-          Documento de identidad
-        </label>
-        <input
-          type="text"
-          className="bg-light border border-none text-dark p-2 mb-3"
-          placeholder="Documento de identidad"
-          required
-        />
+      <form onSubmit={handleSubmit} className="d-flex flex-column gap-2">
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Documento de identidad
+          </label>
+          <input
+            type="text"
+            className="bg-light border border-none text-dark p-2 mb-3"
+            placeholder="Documento de identidad"
+            name="personalId"
+            value={state.personalId}
+            onChange={handleChange}
+          />
+          {errors.personalId && <p className={style.errorText}>{errors.personalId}</p>}
+        </div>
 
-        <label htmlFor="" className="fst-italic align-self-start">
-          Contraseña
-        </label>
-        <input type="password" className="bg-light border border-none text-dark p-2" placeholder="Contraseña" required />
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            className="bg-light border border-none text-dark p-2"
+            placeholder="Contraseña"
+            name="password"
+            value={state.password}
+            onChange={handleChange}
+          />
+          {errors.password && (
+            <p className={style.errorText}>
+              {errors.password.map((elem, index) => (
+                <p key={index}>{elem}</p>
+              ))}
+            </p>
+          )}
+        </div>
 
-        <a href="#" className="link-underline link-underline-opacity-0 fs-6 align-self-end mb-2">
+        <Link to="/resetPassword" className="link-underline link-underline-opacity-0 fs-6 align-self-end mb-2">
           Olvidé mi contraseña
-        </a>
+        </Link>
         <button className="btn btn-success my-2 mx-auto w-50">Ingresar</button>
         <p href="#" className="mt-2">
           ¿ No tienes cuenta ?
--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -1,175 +1,330 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useServices from "../../services/useServices";
+import validatePassword from "../../utilities/validatePassword";
+import style from "./register.module.css";
+
+const initialValues = {
+  personalId: "",
+  password: "",
+  password2: "",
+  name: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  birthDay: "",
+  gender: "",
+  address: "",
+  nationality: "",
+  cp: "",
+  conditions: false,
+  robot: false,
+};
+
 const Register = () => {
-    return (
-        <main className="container mx-auto p-4" >
-            <h1>Crear nueva cuenta</h1>
-            <p className="mb-5">Forem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <form action="" className="d-flex flex-column gap-2 align-self-start text-start">
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Nombre/s
-                </label>
-                <input
-                    type="text"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Nombre/s"
-                    required
-                />
+  const [state, setState] = useState(initialValues);
+  const [errors, setErrors] = useState({});
 
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Apellido/s
-                </label>
-                <input
-                    type="text"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Apellido/s"
-                    required
-                />
+  const navigate = useNavigate();
+  const { auth } = useServices();
 
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Documento de identidad
-                </label>
-                <input
-                    type="text"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Documento de identidad"
-                    required
-                />
+  const validate = () => {
+    let errorsList = {};
+    const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-                <div className="row">
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            Fecha de Nacimiento
-                        </label>
-                        <input
-                            type="date"
-                            className="form-control bg-light border border-none text-dark p-2"
-                            placeholder="Fecha de Nacimiento"
-                            required
-                        />
-                    </div>
+    if (!state.email) errorsList = { ...errorsList, email: "Campo obligatorio" };
+    else if (!emailRegExp.test(state.email)) errorsList = { ...errorsList, email: "Debe ingresar una direccion de correo valida" };
+    if (!state.name) errorsList = { ...errorsList, name: "Campo obligatorio" };
+    if (!state.lastName) errorsList = { ...errorsList, lastName: "Campo obligatorio" };
+    if (!state.personalId) errorsList = { ...errorsList, personalId: "Campo obligatorio" };
+    else if (state.personalId.length < 8) errorsList = { ...errorsList, personalId: "Debe tener al menos 8 digitos" };
+    if (!state.birthDay) errorsList = { ...errorsList, birthDay: "Campo obligatorio" };
+    if (!state.nationality) errorsList = { ...errorsList, nationality: "Campo obligatorio" };
+    if (!state.gender) errorsList = { ...errorsList, gender: "Campo obligatorio" };
+    if (!state.phoneNumber) errorsList = { ...errorsList, phoneNumber: "Campo obligatorio" };
+    if (!state.address) errorsList = { ...errorsList, address: "Campo obligatorio" };
+    if (!state.cp) errorsList = { ...errorsList, cp: "Campo obligatorio" };
 
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            Nacionalidad
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control bg-light border border-none text-dark p-2"
-                            placeholder="Nacionalidad"
-                            required
-                        />
-                    </div>
-                </div>
+    const errorPass1 = validatePassword(state.password);
+    const errorPass2 = validatePassword(state.password2);
 
-                <div className="row">
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            Sexo
-                        </label>
-                        <select
-                            className="form-select bg-light border border-none text-dark p-2"
-                            required
-                        >
-                            <option value="">Seleccionar sexo</option>
-                            <option value="male">Masculino</option>
-                            <option value="female">Femenino</option>
-                        </select>
-                    </div>
+    if (errorPass1) errorsList = { ...errorsList, password: errorPass1 };
+    if (errorPass2) errorsList = { ...errorsList, password2: errorPass2 };
+    if (!errorPass1 & !errorPass2 && state.password !== state.password2)
+      errorsList = { ...errorsList, password: ["Los passwords deben coincidir"], password2: ["Los passwords deben coincidir"] };
 
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            Número de teléfono
-                        </label>
-                        <input
-                            type="tel"
-                            className="form-control bg-light border border-none text-dark p-2"
-                            placeholder="Número de teléfono"
-                            required
-                        />
-                    </div>
-                </div>
+    if (!state.conditions) errorsList = { ...errorsList, conditions: "Debe aceptar los terminos y condiciones" };
+    if (!state.robot) errorsList = { ...errorsList, robot: "Aceptar que no es un robot" };
+    return errorsList;
+  };
 
-                <div className="row">
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            Dirección
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control bg-light border border-none text-dark p-2"
-                            placeholder="Dirección"
-                            required
-                        />
-                    </div>
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
-                    <div className="col-md-6 col-6">
-                        <label htmlFor="" className="fst-italic align-self-start">
-                            CP
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control bg-light border border-none text-dark p-2"
-                            placeholder="CP"
-                            required
-                        />
-                    </div>
-                </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors({});
+    const response = validate();
+    if (Object.keys(response).length < 1) {
+      const body = {
+        name: `${state.name} ${state.lastName}`,
+        password: state.password,
+        personalId: state.personalId,
+        email: state.email,
+        phoneNumber: state.phoneNumber,
+        address: state.address,
+        birthDay: Date.parse(state.birthDay),
+        gender: state.gender,
+      };
+      try {
+        const { data } = await auth.signup(body);
+        console.log(data.message);
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setErrors(response);
+  };
 
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Email
-                </label>
-                <input
-                    type="email"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Email"
-                    required
-                />
+  return (
+    <main className="container mx-auto p-4" onSubmit={handleSubmit}>
+      <h1>Crear nueva cuenta</h1>
+      <p className="mb-5">Forem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <form action="" className="d-flex flex-column gap-2 align-self-start text-start">
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Nombre/s
+          </label>
+          <input
+            type="text"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Nombre/s"
+            name="name"
+            value={state.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p className={style.errorText}>{errors.name}</p>}
+        </div>
 
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Contraseña
-                </label>
-                <input
-                    type="password"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Contraseña"
-                    required
-                />
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Apellido/s
+          </label>
+          <input
+            type="text"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Apellido/s"
+            name="lastName"
+            value={state.lastName}
+            onChange={handleChange}
+          />
+          {errors.lastName && <p className={style.errorText}>{errors.lastName}</p>}
+        </div>
 
-                <label htmlFor="" className="fst-italic align-self-start">
-                    Repetir contraseña
-                </label>
-                <input
-                    type="password"
-                    className="form-control bg-light border border-none text-dark p-2"
-                    placeholder="Repetir contraseña"
-                    required
-                />
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Documento de identidad
+          </label>
+          <input
+            type="text"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Documento de identidad"
+            name="personalId"
+            value={state.personalId}
+            onChange={handleChange}
+          />
+          {errors.personalId && <p className={style.errorText}>{errors.personalId}</p>}
+        </div>
 
-                <label style={{ marginTop: "15px" }} className="bg-white">
-                    <input type="checkbox" className="form-check-input" />
-                    Acepto los términos y condiciones
-                </label>
+        <div className="row">
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              Fecha de Nacimiento
+            </label>
+            <input
+              type="date"
+              className="form-control bg-light border border-none text-dark p-2"
+              placeholder="Fecha de Nacimiento"
+              name="birthDay"
+              value={state.birthDay}
+              onChange={handleChange}
+            />
+            {errors.birthday && <p className={style.errorText}>{errors.birthday}</p>}
+          </div>
 
-                <label className="bg-white">
-                    <input type="checkbox" className="form-check-input" />
-                    Acepto recibir notificaciones vía mail (opcional)
-                </label>
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              Nacionalidad
+            </label>
+            <input
+              type="text"
+              className="form-control bg-light border border-none text-dark p-2"
+              placeholder="Nacionalidad"
+              name="nationality"
+              value={state.nationality}
+              onChange={handleChange}
+            />
+            {errors.nationality && <p className={style.errorText}>{errors.nationality}</p>}
+          </div>
+        </div>
 
-                <div className="border p-3 d-flex align-items-center">
-                    <label className="m-0">
-                        <input type="checkbox" className="form-check-input" />
-                        No soy un robot
-                    </label>
-                </div>
+        <div className="row">
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              Sexo
+            </label>
+            <select
+              className="form-select bg-light border border-none text-dark p-2"
+              name="gender"
+              value={state.gender}
+              onChange={handleChange}
+            >
+              <option value="">Seleccionar sexo</option>
+              <option value="male">Masculino</option>
+              <option value="female">Femenino</option>
+            </select>
+            {errors.gender && <p className={style.errorText}>{errors.gender}</p>}
+          </div>
 
-                <button
-                    className="btn btn-primary my-3 bg-success w-50 mx-auto border-0"
-                    style={{ transition: "background-color 0.2s", backgroundColor: "#000" }}
-                >
-                    Enviar
-                </button>
-            </form>
-        </main>
-    );
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              Número de teléfono
+            </label>
+            <input
+              type="tel"
+              className="form-control bg-light border border-none text-dark p-2"
+              placeholder="Número de teléfono"
+              name="phoneNumber"
+              value={state.phoneNumber}
+              onChange={handleChange}
+            />
+            {errors.phoneNumber && <p className={style.errorText}>{errors.phoneNumber}</p>}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              Dirección
+            </label>
+            <input
+              type="text"
+              className="form-control bg-light border border-none text-dark p-2"
+              placeholder="Dirección"
+              name="address"
+              value={state.address}
+              onChange={handleChange}
+            />
+            {errors.address && <p className={style.errorText}>{errors.address}</p>}
+          </div>
+
+          <div className="col-md-6 col-6">
+            <label htmlFor="" className="fst-italic align-self-start">
+              CP
+            </label>
+            <input
+              type="text"
+              className="form-control bg-light border border-none text-dark p-2"
+              placeholder="CP"
+              name="cp"
+              value={state.cp}
+              onChange={handleChange}
+            />
+            {errors.cp && <p className={style.errorText}>{errors.cp}</p>}
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Email"
+            name="email"
+            value={state.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className={style.errorText}>{errors.email}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Contraseña"
+            name="password"
+            value={state.password}
+            onChange={handleChange}
+          />
+          {errors.password && (
+            <p className={style.errorText}>
+              {errors.password.map((elem, index) => (
+                <p key={index}>{elem}</p>
+              ))}
+            </p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="" className="fst-italic align-self-start">
+            Repetir contraseña
+          </label>
+          <input
+            type="password"
+            className="form-control bg-light border border-none text-dark p-2"
+            placeholder="Repetir contraseña"
+            name="password2"
+            value={state.password2}
+            onChange={handleChange}
+          />
+          {errors.password2 && (
+            <p className={style.errorText}>
+              {" "}
+              {errors.password2.map((elem, index) => (
+                <p key={index}>{elem}</p>
+              ))}
+            </p>
+          )}
+        </div>
+
+        <div>
+          <label style={{ marginTop: "15px" }} className="bg-white">
+            <input type="checkbox" className="form-check-input" name="conditions" value={state.conditions} onChange={handleChange} />
+            Acepto los términos y condiciones
+          </label>
+          {errors.conditions && <p className={style.errorText}>{errors.conditions}</p>}
+        </div>
+
+        {/* <label className="bg-white">
+          <input type="checkbox" className="form-check-input" />
+          Acepto recibir notificaciones vía mail (opcional)
+        </label> */}
+
+        <div className="border p-3 d-flex align-items-center">
+          <label className="m-0">
+            <input type="checkbox" className="form-check-input" name="robot" value={state.robot} onChange={handleChange} />
+            No soy un robot
+          </label>
+          {errors.robot && <p className={style.errorText}>{errors.robot}</p>}
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary my-3 bg-success w-50 mx-auto border-0"
+          style={{ transition: "background-color 0.2s", backgroundColor: "#000" }}
+        >
+          Registrarse
+        </button>
+      </form>
+    </main>
+  );
 };
 
 export default Register;
