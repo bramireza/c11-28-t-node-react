b1a33f9f0d5046962fabab86e9977c3eb8496fcc
Brayian <bramirezag@gmail.com>
2023-05-24 15:47:24 -0500

    [feat] Update routes

--- Backend/src/routes/doctorRouter.js
+++ Backend/src/routes/doctorRouter.js
@@ -5,10 +5,15 @@ const isAuthenticated = require("../middlewares/authJwt");
 const doctorRouter = express.Router();
 
 doctorRouter.post("/", isAuthenticated, doctorController.store);
+
+doctorRouter.get("/", isAuthenticated, doctorController.getAll);
 doctorRouter.get(
-  "/:idSpecialty",
+  "/specialty/:idSpecialty",
   isAuthenticated,
   doctorController.getOneBySpecialty
 );
-doctorRouter.get("/", isAuthenticated, doctorController.getAll);
+doctorRouter.get("/:id", isAuthenticated, doctorController.getOne);
+doctorRouter.put("/:id", isAuthenticated, doctorController.update);
+doctorRouter.delete("/:id", isAuthenticated, doctorController.remove);
+
 module.exports = doctorRouter;
