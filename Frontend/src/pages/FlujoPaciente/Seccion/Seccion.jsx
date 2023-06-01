8213bf2ee26f8175223a0890e34684d83678a7a9
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-01 03:05:16 -0300

    se actualiza datos de paciente y doctor por la base de datos

--- Frontend/src/pages/FlujoPaciente/Seccion/Seccion.jsx
+++ Frontend/src/pages/FlujoPaciente/Seccion/Seccion.jsx
@@ -11,30 +11,64 @@ const headers = accessToken
 
 
 const Seccion = ()=>{
-    const [nombre, setNombre] = useState('');
-    
-    
-    
-    api().get("/auth/me", { headers})
-        .then(res => setNombre(res.data.user.name))
-        .catch(err => console.log(err))
-    
+    const [nompa, setNompa] = useState('');
+    const [doc, setDoc] = useState('');
+    const [idDoc, setIdDoc] = useState('');
+    const [area, setArea] = useState('');
+    const [dia, setDia] = useState('');
+    const [hora, setHora] = useState('');
+
+
+
+        //Datos del Paciente
+        const paciente = ()=>{
+            api().get('appointment/me', { headers })
+            .then(res =>{
+                setHora(res.data.appointments[0].appointmentDate.split('T')[1].slice(0,5)),
+                setDia(res.data.appointments[0].appointmentDate.split('T')[0])
+        })
+            .catch(err => console.log(err))
+        }
+        paciente();
+
+        // Datos del doctor
+        const doctor = (id)=>{
+            api().get(`/doctor/${id}`, { headers })
+            .then(res =>
+                 {
+                setArea(res.data.doctor.specialties[0].name),
+                setDoc(res.data.doctor.name)}
+                )
+            .catch(err => err)
+        }
+        doctor(idDoc)
+        
+        api().get("/auth/me", { headers})
+            .then(res => setNompa(res.data.user.name))
+            .catch(err => console.log(err))
+
+        api().get("/appointment/me", { headers})
+            .then(res => 
+                setIdDoc(res.data.appointments[0].doctor)
+                )
+            .catch(err => console.log(err))
+            
     return(
         <>
         
         <header className="header d-flex p-3 align-items-center justify-content-start gap-2">
             <img src="https://www.freeiconspng.com/thumbs/account-icon/account-profile-icon-1.png" className="img_perfil img-fluid" alt="perfil" />
-            <h5 className='saludo '>Hola, {nombre}.</h5>
+            <h5 className='saludo '>Hola, {nompa}.</h5>
         </header>
         <main>
             <section className='p-3'>
                 <h3>Turnos</h3>
                 <article className='d-flex p-3 align-items-center justify-content-between gap-4'>
                     <div className="datos_turno">
-                        <h4 className='py-2'>Dr.Felipe Sanchez</h4>
-                        <p>Área: Ginecología</p>
-                        <p>Día: Martes 10/05/23</p>
-                        <p>Horarios: 9:00 am - 9:30 am</p>
+                        <h4 className='py-2'>Dr.{doc}</h4>
+                        <p>Área: {area}</p>
+                        <p>Día: {dia}</p>
+                        <p>Horario: {hora}</p>
                     </div>
                         <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg" className='img_turno' alt="logoDoc" />
                 </article>
@@ -47,7 +81,7 @@ const Seccion = ()=>{
                             <h4 className='py-2'>Dra.Lupita Ortega</h4>
                             <p>Área: Ginecología</p>
                             <p>Día: Martes 10/05/23</p>
-                            <p>Horarios: 9:00 am - 9:30 am</p>
+                            <p>Horario: 9:00 am - 9:30 am</p>
                         </div>
                     </article>
                     <article className='d-flex p-3 align-items-center justify-content-between gap-4'>
@@ -56,7 +90,7 @@ const Seccion = ()=>{
                             <h4 className='py-2'>Dr.Pedro Aquino</h4>
                             <p>Área: Clinico</p>
                             <p>Día: Martes 08/05/23</p>
-                            <p>Horarios: 19:00 pm - 19:30 pm</p>
+                            <p>Horario: 19:00 pm - 19:30 pm</p>
                         </div>
                     </article>
             </section>
