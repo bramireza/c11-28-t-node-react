4be0ea8e4163e9fc8e7a6c83d383e30bc7d2b097
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2023-06-05 19:35:25 -0300

    estilo turnos

---
+++ Frontend/src/pages/MensajeAnteriorEspecialidad/MensajeAnteriorEspecialidad.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MensajeAnteriorEspecialidad.css";
+
+const MensajeAnteriorEspecialidad = () => {
+  return (
+    <div className='mensaje'>Por Favor seleccione en el desplegable la especialidad para poder solicitar su turno medico.</div>
+  )
+}
+
+export default MensajeAnteriorEspecialidad
