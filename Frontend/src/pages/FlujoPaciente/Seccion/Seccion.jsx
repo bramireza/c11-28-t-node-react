27d0ec15d970150fe971972dc865760a03e582b4
Brayian <bramirezag@gmail.com>
2023-06-05 00:44:22 -0500

    [fix] Solution in dependencies and component seccion

--- Frontend/src/pages/FlujoPaciente/Seccion/Seccion.jsx
+++ Frontend/src/pages/FlujoPaciente/Seccion/Seccion.jsx
@@ -1,144 +1,155 @@
-import './seccion.css';
-import { api } from '../../../utilities/axios';
-import { useState, useEffect } from 'react';
+import "./seccion.css";
+import { api } from "../../../utilities/axios";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const accessToken = localStorage.getItem("accessToken");
 const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : {};
 
 const Seccion = () => {
-    const [nombre, setNombre] = useState("");
+  const [user, setUser] = useState({});
+  const [pastAppointments, setPastAppoinments] = useState([]);
+  const [upcomingAppointments, setUpcomingAppoinments] = useState([]);
 
-    useEffect(() => {
-        api()
-            .get("/auth/me", { headers })
-            .then((res) => setNombre(res.data.user.name))
-            .catch((err) => console.log(err));
-    }, []);
+  //Datos del Paciente
+  useEffect(() => {
+    api()
+      .get("/auth/me", { headers })
+      .then((res) => setUser(res.data.user))
+      .catch((err) => console.log(err));
+  }, []);
 
+  // Formateador de fechas de citas medicas
+  function formatAppoinments(data, setMethod) {
+    const formatData = data.map((esp) => {
+      const date = new Date(esp.appointmentDate);
+      const appointmentDate = date.toLocaleDateString();
+      const startTime = date.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "numeric",
+      });
+      // Sumamos hora inico + duracion cita
+      date.setMinutes(date.getMinutes() + esp.duration);
+      const endTime = date.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "numeric",
+      });
+      return {
+        ...esp,
+        appointmentDate,
+        startTime,
+        endTime,
+      };
+    });
+    setMethod(formatData);
+  }
+  // Citas medicas pasadas
+  useEffect(() => {
+    api()
+      .get("/appointment/me/past", { headers })
+      .then((res) =>
+        formatAppoinments(res.data.appointments, setPastAppoinments)
+      )
+      .catch((err) => console.log(err));
+  }, []);
+  // Citas medicas proximas
+  useEffect(() => {
+    api()
+      .get("/appointment/me/upcoming", { headers })
+      .then((res) =>
+        formatAppoinments(res.data.appointments, setUpcomingAppoinments)
+      )
+      .catch((err) => console.log(err));
+  }, []);
 
-
-    // const [nompa, setNompa] = useState('');
-    const [doc, setDoc] = useState('');
-    const [idDoc, setIdDoc] = useState('');
-    const [area, setArea] = useState('');
-    const [dia, setDia] = useState('');
-    const [hora, setHora] = useState('');
-
-
-
-        //Datos del Paciente
-        useEffect(
-            ()=>{
-                api().get('appointment/me', { headers })
-                .then(res =>{
-                    setHora(res.data.appointments[0].appointmentDate.split('T')[1].slice(0,5))
-                    setDia(res.data.appointments[0].appointmentDate.split('T')[0])
-            })
-                .catch(err => console.log(err))
-            }
-
-        ),[];
-        
-
-        // Datos del doctoR
-        useEffect(
-            ()=>{
-                api().get(`/doctor/${idDoc}`, { headers })
-                .then(res =>
-                     {
-                    setArea(res.data.doctor.specialties[res.data.doctor.specialties.length - 1].name),
-                    setDoc(res.data.doctor.name)}
-                    )
-                .catch(err => err)
-            }
-        ),[idDoc];
-        
-        
-        // Busca nombre del paciente
-        useEffect(
-            ()=>{
-                api().get("/auth/me", { headers})
-                    .then(res => setNompa(res.data.user.name))
-                    .catch(err => console.log(err))
-            }
-
-        ),[];
-        
-        //Busca el id del doctor
-        useEffect(
-            ()=>{
-                api().get("/appointment/me", { headers})
-                    .then(res => {
-                        // console.log(res.data.appointments[0].doctor)
-                        setIdDoc(res.data.appointments[0].doctor)
-                    }
-                        )
-                    .catch(err => console.log(err))
-            }
-        ),[];
-        
-            
-            
-    return(
-
-        <>
-
-            <header className="header d-flex p-3 align-items-center justify-content-start gap-2">
-                <img src="https://www.freeiconspng.com/thumbs/account-icon/account-profile-icon-1.png" className="img_perfil img-fluid rounded-circle" alt="perfil" />
-                <h5 className='col-6 '>Hola, {nombre.toUpperCase()}.</h5>
-            </header>
-            <main>
-                <section className='p-3'>
-                    <h3>Turno reciente</h3>
-                    <article className='d-flex p-3 align-items-center justify-content-between gap-4 bg-body-tertiary shadow'>
-                        <div className="datos_turno">
-                            <h4 className='py-2'>Dr.{doc}</h4>
-                            <p>Área: {area}</p>
-                            <p>Día: {dia}</p>
-                            <p>Horario: {hora}</p>
-                        </div>
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg" className='img_turno rounded' alt="logoDoc" />
-                    </article>
-                </section>
-                <section className='p-3'>
-                    <h3>Turnos Anteriores</h3>
-                    <article className='d-flex p-3 align-items-center justify-content-between gap-4 mb-3 bg-body-tertiary shadow '>
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg" className='img_turno rounded' alt="logoDoc" />
-                        <div className="datos_turno">
-                            <h4 className='py-2'>Dra.Lupita Ortega</h4>
-                            <p>Área: Ginecología</p>
-                            <p>Día: Martes 10/05/23</p>
-                            <p>Horario: 9:00 am - 9:30 am</p>
-                        </div>
-                    </article>
-                    <article className='d-flex p-3 align-items-center justify-content-between gap-4 bg-body-tertiary shadow'>
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg" className='img_turno rounded' alt="logoDoc" />
-                        <div className="datos_turno">
-                            <h4 className='py-2'>Dr.Pedro Aquino</h4>
-                            <p>Área: Clinico</p>
-                            <p>Día: Martes 08/05/23</p>
-                            <p>Horario: 19:00 pm - 19:30 pm</p>
-                        </div>
-                    </article>
-                </section>
-                <div className='d-flex px-3 pb-3'>
-                    <Link to='/turnos-especialidad' className='btn btn-success col-12 col-md-6 mx-auto'>Crear nuevo turno</Link>
-
-                </div>
-
-
-                <div className="d-flex px-3 pb-3">
-                    <Link
-                        to="/turnos-especialidad"
-                        className="btn btn-success col-12 col-md-6 mx-auto"
+  return (
+    <>
+      <header className="header d-flex p-3 align-items-center justify-content-start gap-2">
+        <img
+          src="https://www.freeiconspng.com/thumbs/account-icon/account-profile-icon-1.png"
+          className="img_perfil img-fluid rounded-circle"
+          alt="perfil"
+        />
+        <h5 className="col-6 ">Hola, {user.name?.toUpperCase()}.</h5>
+      </header>
+      <main>
+        <section className="p-3">
+          <h3>Turnos activos</h3>
+          <p>Estas son tus próximas citas confirmadas:</p>
+          <div
+            className="d-flex flex-row flex-nowrap"
+            style={{ overflowX: "auto" }}
+          >
+            {upcomingAppointments.length != 0
+              ? upcomingAppointments.map((apt) => {
+                  return (
+                    <article
+                      className="d-flex p-3 justify-content-between gap-4 bg-body-tertiary shadow"
+                      key={apt._id}
+                      style={{
+                        width: "500px",
+                        minWidth: "400px",
+                        margin: "0 10px",
+                      }}
                     >
-                        Crear nuevo turno
-                    </Link>
-                </div>
-            </main>
-        </>
-    );
+                      <img
+                        src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg"
+                        className="img_turno rounded"
+                        alt="logoDoc"
+                        style={{ maxWidth: "100%" }}
+                      />
+                      <div className="datos_turno">
+                        <h4 className="py-2">Dr.{apt.doctor.name}</h4>
+                        <p>Área: {apt.specialty.name}</p>
+                        <p>Día: {apt.appointmentDate}</p>
+                        <p>
+                          Horario: {apt.startTime} - {apt.endTime}
+                        </p>
+                      </div>
+                    </article>
+                  );
+                })
+              : "Sin turnos pendientes"}
+          </div>
+        </section>
+        <section className="p-3">
+          <h3>Turnos pasados</h3>
+          {pastAppointments.length != 0
+            ? pastAppointments.map((apt) => {
+                return (
+                  <article
+                    className="d-flex p-3 align-items-center justify-content-between gap-4 mb-3 bg-body-tertiary shadow "
+                    key={apt._id}
+                  >
+                    <img
+                      src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/7da02324714817.56338d63f2a30.jpg"
+                      className="img_turno rounded"
+                      alt="logoDoc"
+                    />
+                    <div className="datos_turno">
+                      <h4 className="py-2">Dr.{apt.doctor.name}</h4>
+                      <p>Área: {apt.specialty.name}</p>
+                      <p>Día: {apt.appointmentDate}</p>
+                      <p>
+                        Horario: {apt.startTime} - {apt.endTime}
+                      </p>
+                    </div>
+                  </article>
+                );
+              })
+            : "Sin turnos "}
+        </section>
+        <div className="d-flex px-3 pb-3">
+          <Link
+            to="/turnos-especialidad"
+            className="btn btn-success col-12 col-md-6 mx-auto"
+          >
+            Crear nuevo turno
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Seccion;
