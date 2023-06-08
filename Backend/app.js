d1ea68c2356ae1876b0c2d8c398d1bc4f731342a
Brayian <bramirezag@gmail.com>
2023-06-07 22:52:56 -0500

    [fix] solution access control

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,15 @@ mongoose
     console.log(err);
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
