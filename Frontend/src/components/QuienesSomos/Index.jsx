858ceaf7076de386fbdc693e58e0e3c21a48d85b
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-12 01:32:56 -0300

    se arreglo somos

--- Frontend/src/components/QuienesSomos/Index.jsx
+++ Frontend/src/components/QuienesSomos/Index.jsx
@@ -1,14 +1,15 @@
 import './style.css';
-import imgsomos from '../../assets/imgquienes.png';
+// import imgsomos from '../../assets/imgquienes.png';
 import cruz from '../../assets/cruzsomos.svg';
 
 
 function QuienesSomos() {
   return (
     <>
-      
-        <div className='contimg '>
-            <img src={imgsomos} alt="" className='imgquienesSomos mx-auto' />
+      <div className="somos">
+
+        <div className='contimg'>
+            {/* <img src={imgsomos} alt="" className='imgquienesSomos mx-auto' /> */}
             <div className='cuadroAzul px-3'>
               <h2 className='mt-4'>¿Quienes somos?</h2>
               <p className='mt-3'>Somos Syncare Hospital, una institución de salud comprometida con el bienestar y la atención médica de calidad. Nuestro equipo de profesionales altamente capacitados y nuestra infraestructura de vanguardia nos permiten brindar servicios médicos de excelencia a nuestros pacientes. 
@@ -19,6 +20,7 @@ function QuienesSomos() {
           
             </div>
         </div>
+      </div>
 
       
     </>
