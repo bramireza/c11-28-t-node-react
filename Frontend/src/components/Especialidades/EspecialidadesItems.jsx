b4408650959163401bc34115b46c150372628045
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-14 10:24:58 -0300

    se agregaron img de fondo en especialidades y se maqueto como el diseño en figma

--- Frontend/src/components/Especialidades/EspecialidadesItems.jsx
+++ Frontend/src/components/Especialidades/EspecialidadesItems.jsx
@@ -8,45 +8,49 @@ const EspecialidadesItems = ()=>{
     'Otorrinolaringología'];
 
     return <>
-        <div className="" style={{ 
-                    backgroundColor: "#1043A5",
-                    borderTopLeftRadius: "30px",
-                    borderTopRightRadius: "30px",
-                    marginTop: "30px",
-                    marginInline: "30px",
-                    padding: "30px"
-            }}>
-            <h2 className="fw-bolder" style={{ 
-                    color: "#FAFAFA",
-                    fontFamily: "'Poppins', 'sans-serif'",
-                    fontWeight: '600',
-                    fontSize: '32px',
-                    lineHeight: '150%',
-            }}>Especialidades</h2>
-            <p style={{ 
-                    color: "#FAFAFA",
-                    fontFamily: "'Poppins', 'sans-serif'",
-                    fontWeight: "400",
-                    fontSize: "16px",
-                    lineHeight: "150%",
-            }}>Descubre nuestras especialidades médicas, con un equipo experto que brinda atención especializada y cuidado integral para tu salud.</p>
-        </div>
-        <div className="px-4 d-flex flex-wrap" style={{ 
-                    backgroundColor: "#1043A5",
-                    borderBottomLeftRadius: "30px",
-                    borderBottomRightRadius: "30px",
-                    marginInline: "30px",
-                    padding: "30px"
-            }}>
-            {listaCategorias.map((data,index)=>{
-                return <Especialidades
-                    key={index}
-                    especialidad={data}
+    <div className="imgFondo mx-auto">
+            <div className="cuadroAzulParrafo " style={{ 
+                        backgroundColor: "#1043A5",
+                        borderTopLeftRadius: "30px",
+                        borderTopRightRadius: "30px",
+                        marginTop: "30px",
+                        marginInline: "30px",
+                        padding: "30px"
+                        
+                }}>
+                <h2 className="fw-bolder" style={{ 
+                        color: "#FAFAFA",
+                        fontFamily: "'Poppins', 'sans-serif'",
+                        fontWeight: '600',
+                        fontSize: '32px',
+                        lineHeight: '150%',
+                        paddingRight:'20px'
+                }}>Especialidades</h2>
+                <p style={{ 
+                        color: "#FAFAFA",
+                        fontFamily: "'Poppins', 'sans-serif'",
+                        fontWeight: "400",
+                        fontSize: "16px",
+                        lineHeight: "150%",
+                }}>Descubre nuestras especialidades médicas, con un equipo experto que brinda atención especializada y cuidado integral para tu salud.</p>
+            </div>
+            <div className=" d-flex flex-wrap cuadradoAzul" style={{ 
+                        backgroundColor: "#1043A5",
+                        borderBottomLeftRadius: "30px",
+                        borderBottomRightRadius: "30px",
+                        marginInline: "30px",
+                        // padding: "30px"
+                }}>
+                {listaCategorias.map((data,index)=>{
+                    return <Especialidades
+                        key={index}
+                        especialidad={data}
 
-                />
+                    />
 
-            })}
-        </div>
+                })}
+            </div>
+    </div>
 
     </>
 }
