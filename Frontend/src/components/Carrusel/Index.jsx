d12c138468356899a7c4f883de9da083193eb772
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-14 20:20:08 -0300

    se acomodo el mapa y se agregaron las img para movil en el carrusel

--- Frontend/src/components/Carrusel/Index.jsx
+++ Frontend/src/components/Carrusel/Index.jsx
@@ -1,11 +1,27 @@
 import Carousel from 'react-bootstrap/Carousel';
 import './style.css';
 import { Link } from 'react-router-dom';
-import Banner1  from '../../assets/banner1.png';
-import Banner2  from '../../assets/banner2.png';
-import Banner3  from '../../assets/banner3.png';
+import { useState, useEffect } from 'react';
+import Banner1desktop  from '../../assets/banner1.png';
+import Banner2desktop  from '../../assets/banner2.png';
+import Banner3desktop  from '../../assets/banner3.png';
+import Banner1movil from '../../assets/Banner1movil.png';
+import Banner2movil from '../../assets/Banner2movil.png';
+import Banner3movil from '../../assets/Banner3movil.png';
 
 function IndividualIntervalsExample() {
+  const [banner1, setBanner1] = useState(Banner1movil);
+  const [banner2, setBanner2] = useState(Banner2movil);
+  const [banner3, setBanner3] = useState(Banner3movil);
+  useEffect(()=>{
+    const ancho = window.screen.width;
+    if(ancho > 576){
+      setBanner1(Banner1desktop)
+      setBanner2(Banner2desktop)
+      setBanner3(Banner3desktop);
+    }
+  },[])
+
   return (
     <div className='contenedor'>
 
@@ -13,7 +29,7 @@ function IndividualIntervalsExample() {
         <Carousel.Item interval={1000}>
           <img
             className="d-block w-100 img_caruusel"
-            src={Banner1}
+            src={banner1}
             alt="First slide"
           />
           <Carousel.Caption className='primera'>
@@ -25,7 +41,7 @@ function IndividualIntervalsExample() {
         <Carousel.Item interval={1000}>
           <img
             className="d-block w-100 img_caruusel"
-            src={Banner2}
+            src={banner2}
             alt="Second slide"
           />
           <Carousel.Caption className='primera'>
@@ -37,7 +53,7 @@ Especialistas</h3>
         <Carousel.Item>
           <img
             className="d-block w-100 img_caruusel"
-            src={Banner3}
+            src={banner3}
             alt="Third slide"
           />
           <Carousel.Caption className='primera'>
